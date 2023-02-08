21e09cc13d651602b9d9b8478799959a3b91fddf
Murray Leys <murrayleys@murrays-mbp-2.home>
2023-02-08 22:36:55 +0000

    Download and plot data from 3 timeframes on one graph

--- getklinedata.py
+++ getklinedata.py
@@ -5,8 +5,12 @@ import datetime as dt
 
 URL = "https://api.binance.com/api/v3/klines"
 tokenpair = "ETHBUSD"
-interval = "5m"
-PARAMS = {'symbol': tokenpair, 'interval': interval}
+interval_short = "5m"
+interval_medium = "1h"
+interval_long = "6h"
+PARAMS_SHORT = {'symbol': tokenpair, 'interval': interval_short}
+PARAMS_MEDIUM = {'symbol': tokenpair, 'interval': interval_medium}
+PARAMS_LONG = {'symbol': tokenpair, 'interval': interval_long}
 
 # intervals supported: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1mo
 
@@ -26,16 +30,35 @@ PARAMS = {'symbol': tokenpair, 'interval': interval}
 
 
 # download data from Binance
-raw_data = requests.get(url=URL, params=PARAMS)
+# short term data
+raw_data = requests.get(url=URL, params=PARAMS_SHORT)
 json_data = raw_data.json()
 df = pd.DataFrame(json_data, columns=['OpenTime', 'Open', 'High', 'Low', 'Close', 'Volume', 'CloseTime', 'QuoteAssetVolume',
                                       'QtyOfTrades', 'TBBAV', 'TBQAV', 'Ignore'])
 df['Close'] = df['Close'].astype(float)
-df.index = [dt.datetime.fromtimestamp(x / 1000.0) for x in df.CloseTime]
+df['timeindex'] = [dt.datetime.fromtimestamp(x / 1000.0) for x in df.CloseTime]
 
-# print(json_data)  # Check that data is downloaded correctly
-# print(df.dtypes)
+# Medium term price data
+raw_data2 = requests.get(url=URL, params=PARAMS_MEDIUM)
+json_data2 = raw_data2.json()
+df2 = pd.DataFrame(json_data2, columns=['OpenTime', 'Open', 'High', 'Low', 'Close', 'Volume', 'CloseTime', 'QuoteAssetVolume',
+                                        'QtyOfTrades', 'TBBAV', 'TBQAV', 'Ignore'])
+df2['Close'] = df2['Close'].astype(float)
+df2['timeindex'] = [dt.datetime.fromtimestamp(x / 1000.0) for x in df2.CloseTime]
+df2 = df2[df2.timeindex >= df['timeindex'].min()]
+
+# Long term price data
+raw_data3 = requests.get(url=URL, params=PARAMS_LONG)
+json_data3 = raw_data3.json()
+df3 = pd.DataFrame(json_data3, columns=['OpenTime', 'Open', 'High', 'Low', 'Close', 'Volume', 'CloseTime', 'QuoteAssetVolume',
+                                        'QtyOfTrades', 'TBBAV', 'TBQAV', 'Ignore'])
+df3['Close'] = df3['Close'].astype(float)
+df3['timeindex'] = [dt.datetime.fromtimestamp(x / 1000.0) for x in df3.CloseTime]
+df3 = df3[df3.timeindex >= df['timeindex'].min()]
+
 
 # Plot graph of data and compare to correct plot on tradingview.com to verify that the downloaded data is corect
-df["Close"].plot(title=tokenpair + ' :: ' + interval, legend='close')
+ax = df.plot(x='timeindex', y='Close', title=tokenpair + ' :: Price Data', label='Close ' + interval_short)
+df2.plot(ax=ax, x='timeindex', y='Close', label="Close " + interval_medium)
+df3.plot(ax=ax, x='timeindex', y='Close', label="Close " + interval_long)
 plt.show()
